0397958aab07d76dc940807fd8418b76f9a864ec
Wenter <221040113@link.cuhk.edu.cn>
2022-03-19 12:18:05 +0800

    Initial commit

--- mktpfl.py
+++ mktpfl.py
@@ -2,3 +2,7 @@ from market_profile import MarketProfile
 import pandas as pd
 
 
+amzn = pd.read_csv("IF_Data.csv").iloc[:20]
+mp = MarketProfile(amzn)
+
+mp_slice = mp[amzn.index.min():amzn.index.max()]
